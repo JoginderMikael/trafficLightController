4bd20ef62355021cdc100e2b81e5a7bebe637b53
Joginder Mikael <jogindermikael@gmail.com>
2025-10-26 10:01:31 +0300

    Traffic light, Traffic light state

--- src/main/java/org/runapp/TrafficLight.java
+++ src/main/java/org/runapp/TrafficLight.java
@@ -1,5 +1,60 @@
 package org.runapp;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrafficLight {
+
+   private TrafficLightState currentState;
+   private final String name;
+   private final Timer timer;
+   //private TimerTask task;
+
+    public TrafficLight(String name) {
+        this.name = name;
+        this.currentState = TrafficLightState.RED;
+        this.timer = new Timer();
+    }
+
+    public void start(){
+        IO.println("Traffic Light " + this.name + " starting at " + currentState);
+        scheduleNextState();
+    }
+
+    private void scheduleNextState() {
+        int duration = currentState.getDuration();
+        IO.println(this.name + "is now " + currentState + "for" + duration + "ms");
+        timer.schedule(changeState(), duration);
+    }
+
+    public TimerTask changeState() {
+        switch (currentState) {
+            case RED -> {
+                currentState = TrafficLightState.GREEN;
+                break;
+            }
+
+            case GREEN -> {
+                currentState = TrafficLightState.YELLOW;
+                break;
+            }
+
+            case YELLOW -> {
+                currentState = TrafficLightState.RED;
+                break;
+            }
+
+        }
+        scheduleNextState();
+        return null;
+    }
+
+    public void displayState() {
+        IO.println("Traffic Light " + this.name + " is currently " + currentState);
+    }
+
+    public String getCurrentState() {
+        return currentState.name();
+    }
 
 }
--- src/main/java/org/runapp/TrafficLightState.java
+++ src/main/java/org/runapp/TrafficLightState.java
@@ -1,5 +1,16 @@
 package org.runapp;
 
 public enum TrafficLightState {
-    RED, YELLOW, GREEN
+    RED(5000), YELLOW(4000), GREEN(2000);
+
+    private final int duration;
+
+    TrafficLightState(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
+
